f671fc9a18a431a74d8fdbc1f66b7ab499c0a59f
Marcus Pessoa <marcusvictor00@gmail.com>
2023-04-06 22:06:07 -0300

    Estrutura inicial montada

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`)
+}); 
