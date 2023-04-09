f76632fef70caa97c15c57e4c94a93564a778865
Marcus Pessoa <marcusvictor00@gmail.com>
2023-04-09 19:39:42 -0300

    controller de categorias finalizado

--- src/models/Category.models.ts
+++ src/models/Category.models.ts
@@ -1,28 +1,16 @@
 import mongoose from "mongoose";
 
-const Parent = new mongoose.Schema({
-    _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        require: true,
-        ref: 'Category'
-    },
-    name: {
-        type: String,
-        require: true,
-    }
-});
-
 const Category = new mongoose.Schema({
     name: {
         type: String,
         require: true,
     },
     parent: {
-        type: Parent,
+        type: mongoose.Schema.Types.ObjectId,
         require: false,
+        ref: 'Category'
     }
 });
-
 
 
 export default mongoose.model("Category", Category);
